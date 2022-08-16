bf81a2b525b9c17ec5f0eb445a806c074952fbcf
bgamboe <37429521+bgamboe@users.noreply.github.com>
2022-08-15 21:10:06 -0400

    comments

--- main.lua
+++ main.lua
@@ -20,21 +20,23 @@ local syncKnock = 20
 local function JankAA(cmd) --JankAA
     if syncKnock >= 50 then --this is shit i hate it
         sweepSwitch = -sweepSwitch
-        gui.SetValue('rbot.antiaim.base',179 * sweepSwitch)
-        gui.SetValue('rbot.antiaim.base.rotation',20 * sweepSwitch)
         syncKnock = 20
-        gui.SetValue( 'rbot.antiaim.base.lby', 180 * sweepSwitch)
+    
+        gui.SetValue('rbot.antiaim.base',179 * sweepSwitch) -- base rotation (backwards)
+        gui.SetValue('rbot.antiaim.base.rotation',20 * sweepSwitch) -- desync (this moves the REAL from the FAKE, rotates REAL 20 degrees)
+        gui.SetValue( 'rbot.antiaim.base.lby', 180 * sweepSwitch) -- rotates lby
     end
     if syncKnock ~= 50  and globals.TickCount() % 14 == 0 then
         syncKnock = syncKnock + 1
+    
         if syncKnock % 2 == 0 then
-            gui.SetValue('rbot.antiaim.base',180)
-            gui.SetValue('rbot.antiaim.base.rotation',syncKnock * -sweepSwitch)
-            gui.SetValue('rbot.antiaim.base.lby',(math.abs(gui.GetValue( 'rbot.antiaim.base.lby' ) ) - 6) * sweepSwitch)
+            gui.SetValue('rbot.antiaim.base',180) -- base rotation (backwards)
+            gui.SetValue('rbot.antiaim.base.rotation',syncKnock * -sweepSwitch) -- desync (this moves the REAL from the FAKE, rotates REAL 20 degrees)
+            gui.SetValue('rbot.antiaim.base.lby',(math.abs(gui.GetValue( 'rbot.antiaim.base.lby' ) ) - 6) * sweepSwitch) -- rotates lby
         else
-            gui.SetValue('rbot.antiaim.base',180)
-            gui.SetValue('rbot.antiaim.base.rotation',syncKnock * sweepSwitch)
-            gui.SetValue('rbot.antiaim.base.lby',(math.abs(gui.GetValue( 'rbot.antiaim.base.lby' ) ) - 6) * -sweepSwitch)
+            gui.SetValue('rbot.antiaim.base',180) -- base rotation (backwards)
+            gui.SetValue('rbot.antiaim.base.rotation',syncKnock * sweepSwitch) -- desync (this moves the REAL from the FAKE, rotates REAL 20 degrees)
+            gui.SetValue('rbot.antiaim.base.lby',(math.abs(gui.GetValue( 'rbot.antiaim.base.lby' ) ) - 6) * -sweepSwitch) -- rotates lby
         end    
     end
 end --end JankAA
